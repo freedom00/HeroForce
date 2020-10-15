a79b0884bbff3ed21027f6fc70ca3c4a8f1a07b1
Zhaosheng Liu <jonathan888999@163.com>
2020-10-15 12:18:47 -0400

    update the server url getting method.

--- backend/src/main/java/com/ys/backend/config/WebConfig.java
+++ backend/src/main/java/com/ys/backend/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.ys.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,11 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${HeroForce.appServerUrl}")
+    private String appServerUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(appServerUrl)
                 .allowedMethods("PUT", "DELETE", "POST", "GET", "PATCH");
     }
 }
